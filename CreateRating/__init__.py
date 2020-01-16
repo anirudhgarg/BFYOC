4d507def50651747eeb4707d6e06c2cb4efc7e10
Ankit Kumar <ankikuma@microsoft.com>
2020-01-16 13:23:17 -0800

    Update CreateRating to actually create rating!

--- CreateRating/__init__.py
+++ CreateRating/__init__.py
@@ -1,24 +1,59 @@
 import logging
+import requests
+import uuid
+import datetime
+import json
 
 import azure.functions as func
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
+    req_body = req.get_json()
+    prod_id = req_body['productId']
+    productId = requests.get(f'https://serverlessohproduct.trafficmanager.net/api/GetProduct?productId={prod_id}')
+    if productId.status_code != 200:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
+             'Invalid productId',
              status_code=400
         )
+
+    user_id = req_body['userId']
+    userID = requests.get(f'https://serverlessohuser.trafficmanager.net/api/GetUser?userId={user_id}')
+    if userID.status_code != 200:
+        return func.HttpResponse(
+             'Invalid userId',
+             status_code=400
+        )
+
+    uniqueId = str(uuid.uuid4())
+    timestamp = str(datetime.datetime.utcnow())
+    rating = req_body['rating']
+    if (not is_int(rating)) or (int(rating) < 0 or int(rating) > 5):
+        return func.HttpResponse(
+             'rating value is invalid',
+             status_code=400
+        )
+
+    rating_data = {
+        'id' : uniqueId,
+        'userId': req_body['userId'],
+        'productId': req_body['productId'],
+        'timestamp': timestamp,
+        'locationName': req_body['locationName'],
+        'rating': int(req_body['rating']),
+        'userNotes': req_body['userNotes']
+    }
+
+    doc.set(func.Document.from_json(json.dumps(rating_data)))
+    return func.HttpResponse(
+        json.dumps(rating_data),
+        mimetype='application/json',
+    )
+
+def is_int(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
